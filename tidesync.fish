bdf5035c1bec18057932b89ef15cace3e51c4cd9
Matous Kavalik <matous@kavalik.net>
2025-03-02 12:47:24 +0100

    ..

--- tidesync.fish
+++ tidesync.fish
@@ -5,6 +5,9 @@ function tidesync
     if set -q _flag_d
         set -g _flag_d  # Make it global so it's available in other functions
     end
+    if set -q _flag_s
+        set -g _flag_s  # Make it global so it's available in other functions
+    end
 
     function echo_debug
         if set -q _flag_d
@@ -15,15 +18,19 @@ function tidesync
 
     # Load all necessary inernal functions inside this main function,
     # so it does not clutter up fish shell
-    echo_debug "Starting to load functions from /usr/share/tidesync/*.fish"
+    echo_debug "Loading functions from /usr/share/tidesync/*.fish..."
     for f in /usr/share/tidesync/*.fish
         source $f
     end
 
     #Load configuration (user, server and specified directories)
+    echo_debug "Loading configuration file..."
     load_config
 	
-     for dir in $TIDESYNC_DIRS
+	if set -q _flag_d
+	    echo_debug "Start syncing directories..."
+        for dir in $TIDESYNC_DIRS
+            echo_debug "Syncing $dir..."
             set local_dir $$dir_LOCAL
             set remote_dir $$dir_REMOTE
 
@@ -35,5 +42,6 @@ function tidesync
             else
                 echo "[$(date)] Sync from $dir failed!" >> $TIDESYNC_LOG_FILE
             end
+        end
      end
 end
